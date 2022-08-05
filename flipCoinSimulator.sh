585e3d2de5803aed45f163d53ee4175fb7bb09be
Sandesh <sandesh.khadde@gmail.com>
2022-08-05 20:48:19 +0530

    UC2 Print FlipCoin 20 Times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-coin=$((RANDOM%2));
+head=0;
+tail=0;
 
-if [ $coin -eq 1 ]
-then
-	echo "Heads is Winner";
-else
-	echo "Tails is Winner";
-fi
 
+for((i=1;i<=20;i++))
+do
+
+	coin=$((RANDOM%2));
+
+	if [ $coin -eq 1 ]
+	then
+		echo "Heads is Winner";
+
+	else
+		echo "Tails is Winner";
+	fi
+done
